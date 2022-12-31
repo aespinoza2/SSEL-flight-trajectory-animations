dabf0fc2c9dd49273a72fa1f876f693b9cf194f2
Agustin Espinoza <aespinoza@aeronautical.com>
2022-12-31 14:05:52 -0700

    Add FlightGearAnimation object to flight_trajectory_simulations.m

--- flight_trajectory_simulations.m
+++ flight_trajectory_simulations.m
@@ -1,5 +1,45 @@
-%% Create Flight Animations from Trajectory Data
+% Create Flight Animations from Trajectory Data
 %% Import flight trajectory data
-tdata = readNPY("C:\Users\espin\OneDrive - University of Arizona\Documents\ssel\flight-trajectory-simulations\traj.npy");
+tdata = readmatrix('traj.csv');
 
-%% Test
+%% Create a time series object from trajectory data
+ts = timeseries([convang(tdata(:,[3 2]),'deg','rad') ...
+                 tdata(:,4) convang(tdata(:,5:7),'deg','rad')],tdata(:,1));
+ts = [tdata]
+
+%% Use FlightGearAnimation Object to initialize flight animation
+% Open a FlightGearAnimation object
+h = Aero.FlightGearAnimation;
+
+% Set FlightGearAnimation object properties for timeseries
+h.TimeseriesSourceType = 'Timeseries';
+h.TimeseriesSource = ts;
+
+% Set FlightGearAnimation object properties about FlightGear
+h.FlightGearBaseDirectory = 'C:\Program Files\FlightGear';
+h.GeometryModelName = 'HL20';
+h.DestinationIpAddress =  '127.0.0.1';
+h.DestinationPort = '5502';
+
+% Set the desired initial conditions (location and orientation) for FlightGear flight simulator
+h.AirportId = 'KSFO';
+h.RunwayId = '10L';
+h.InitialAltitude = 7224;
+h.InitialHeading = 113;
+h.OffsetDistance = 4.72;
+h.OffsetAzimuth = 0;
+
+h.InstallScenery = true;
+
+% Disable FlightGearShaders
+h.DisableShaders = true;
+
+% Set the seconds of animation data per second of wall-clock time
+h.TimeScaling = 5;
+
+% Check the FlightGearAnimation object properties and their values
+get(h)
+
+
+
+%% Set Flight
